488e4244090142639ffefa46e6aae8b058715f85
Raven <walraaf@gmail.com>
2022-12-09 11:04:00 +0900

    fix tabcontext issue

--- src/components/spec-checker/SpecTable.tsx
+++ src/components/spec-checker/SpecTable.tsx
@@ -1,53 +1,53 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
-import { TabContext } from '@mui/lab';
+import Tabs from '@mui/material/Tabs';
 import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
 import { DataGrid } from '@mui/x-data-grid';
 import { useTheme } from '@mui/material/styles';
 import { DistData2005, DistData20122010, DistData2017, ResData2005, ResData201720122010,
    columnDefs201220102005, columnDefs2017, columnDefsRes2005, columnDefsRes201720122010 } from './SpecData';
+   import {TabPanel, a11yProps, TabPanelProps} from './TabTools';
 
 
 
 export default function SpecTable() {
 
 
-  const [value, setValue] = React.useState('1');
-  const [DistResTab, setDistResTab] = React.useState('1');
   const theme = useTheme();
 
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
+  const [ISOTab, setISOTab] = React.useState<number>(0);
+  const [DistResTab, setDistResTab] = React.useState<number>(0);  
+   
+  const handleChangeISO = (event: React.SyntheticEvent, newValue: number) => {
+    setISOTab(newValue);
   };
-
-  const handleChangeDistRes = (event: React.SyntheticEvent, newValue: string) => {
+  
+  const handleChangeDistRes = (event: React.SyntheticEvent, newValue: number) => {
     setDistResTab(newValue);
   };
+
 
   // tabs from https://mui.com/material-ui/react-tabs/
   // DataGrid from https://mui.com/material-ui/react-table/
   return (
-    <TabContext value= {DistResTab} >
+    <div>
     <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-      <TabList onChange={handleChangeDistRes} aria-label="lab API tabs example">
-        <Tab label="RESIDUAL FUELS" value="1" />
-        <Tab label="DISTILLATE FUELS" value="2" />
-      </TabList>
-    </Box>
-      <TabPanel value="1" sx={{padding: 0,}}>
-      <Box sx={{ width: '100%', maxWidth: 1120, height: 1030, typography: 'body1' }}>
-        <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <TabList onChange={handleChange} aria-label="lab API tabs example">
-              <Tab label="ISO 8217:2017" value="1" />
-              <Tab label="ISO 8217:2012" value="2" />
-              <Tab label="ISO 8217:2010" value="3" />
-              <Tab label="ISO 8217:2005" value="4" />
-            </TabList>
-          </Box>
-          <TabPanel value="1" sx={{padding: 0,}}>
+    <Tabs value={DistResTab} onChange={handleChangeDistRes} aria-label="Distillate and Residuals Tabs" variant="scrollable">
+      <Tab label="RESIDUAL FUELS" {...a11yProps(0)} />
+      <Tab label="DISTILLATE FUELS" {...a11yProps(1)} />
+    </Tabs>
+  </Box>
+    <TabPanel value={DistResTab} index={0} >
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={ISOTab} onChange={handleChangeISO} aria-label="ISO spec tabs" variant="scrollable">
+          <Tab label="ISO 8217:2017"  {...a11yProps(0)}/>
+          <Tab label="ISO 8217:2012"  {...a11yProps(1)}/>
+          <Tab label="ISO 8217:2010"  {...a11yProps(2)}/>
+          <Tab label="ISO 8217:2005"  {...a11yProps(3)}/>
+        </Tabs>
+      </Box>
+      <TabPanel value={ISOTab} index={0} >
             <div className='mx-auto'  style={{maxWidth: 1300, height: 950, paddingTop: 40, paddingBottom: 40}}>
               <DataGrid sx={{background: theme.palette.background.paper}}
                       density='compact'
@@ -57,7 +57,7 @@ export default function SpecTable() {
                   />
             </div>
           </TabPanel>
-          <TabPanel value="2" sx={{padding: 0,}}>
+          <TabPanel value={ISOTab} index={1} >
             <div className='mx-auto'  style={{maxWidth: 1200, height: 805, paddingTop: 40, paddingBottom: 40}}>
               <DataGrid sx={{background: theme.palette.background.paper}}
                       density='compact'
@@ -67,7 +67,7 @@ export default function SpecTable() {
                   />
             </div>
           </TabPanel>
-          <TabPanel value="3" sx={{padding: 0,}}>
+          <TabPanel value={ISOTab} index={2} >
             <div className='mx-auto'  style={{maxWidth: 1200, height: 805, paddingTop: 40, paddingBottom: 40}}>
               <DataGrid sx={{background: theme.palette.background.paper}}
                       density='compact'
@@ -77,7 +77,7 @@ export default function SpecTable() {
                   />
             </div>
           </TabPanel>
-          <TabPanel value="4" sx={{padding: 0,}}>
+          <TabPanel value={ISOTab} index={3} >
             <div className='mx-auto'  style={{maxWidth: 1200, height: 842, paddingTop: 40, paddingBottom: 40}}>
               <DataGrid sx={{background: theme.palette.background.paper}}
                       density='compact'
@@ -87,22 +87,18 @@ export default function SpecTable() {
                   />
             </div>
           </TabPanel>
-        </TabContext>
-      </Box>
     </TabPanel>
 
-    <TabPanel value="2" sx={{padding: 0,}}>
-      <Box sx={{ width: '100%', maxWidth: 1120, height: 1030, typography: 'body1' }}>
-        <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <TabList onChange={handleChange} aria-label="lab API tabs example">
-              <Tab label="ISO 8217:2017" value="1" />
-              <Tab label="ISO 8217:2012" value="2" />
-              <Tab label="ISO 8217:2010" value="3" />
-              <Tab label="ISO 8217:2005" value="4" />
-            </TabList>
-          </Box>
-          <TabPanel value="1" sx={{padding: 0,}}>
+    <TabPanel value={DistResTab} index={1} >
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={ISOTab} onChange={handleChangeISO} aria-label="ISO spec tabs" variant="scrollable">
+          <Tab label="ISO 8217:2017"  {...a11yProps(0)}/>
+          <Tab label="ISO 8217:2012"  {...a11yProps(1)}/>
+          <Tab label="ISO 8217:2010"  {...a11yProps(2)}/>
+          <Tab label="ISO 8217:2005"  {...a11yProps(3)}/>
+        </Tabs>
+      </Box>
+          <TabPanel value={ISOTab} index={0} >
             <div className='mx-auto'  style={{maxWidth: 1010, height: 950, paddingTop: 40, paddingBottom: 40}}>
               <DataGrid sx={{background: theme.palette.background.paper}}
                       density='compact'
@@ -112,7 +108,7 @@ export default function SpecTable() {
                   />
             </div>
           </TabPanel>
-          <TabPanel value="2" sx={{padding: 0,}}>
+          <TabPanel value={ISOTab} index={1} >
             <div className='mx-auto'  style={{maxWidth: 800, height: 805, paddingTop: 40, paddingBottom: 40}}>
               <DataGrid sx={{background: theme.palette.background.paper}}
                       density='compact'
@@ -122,7 +118,7 @@ export default function SpecTable() {
                   />
             </div>
           </TabPanel>
-          <TabPanel value="3" sx={{padding: 0,}}>
+          <TabPanel value={ISOTab} index={2} >
             <div className='mx-auto'  style={{maxWidth: 800, height: 805, paddingTop: 40, paddingBottom: 40}}>
               <DataGrid sx={{background: theme.palette.background.paper}}
                       density='compact'
@@ -132,7 +128,7 @@ export default function SpecTable() {
                   />
             </div>
           </TabPanel>
-          <TabPanel value="4" sx={{padding: 0,}}>
+          <TabPanel value={ISOTab} index={3} >
             <div className='mx-auto'  style={{maxWidth: 800, height: 842, paddingTop: 40, paddingBottom: 40}}>
               <DataGrid sx={{background: theme.palette.background.paper}}
                       density='compact'
@@ -142,9 +138,7 @@ export default function SpecTable() {
                   />
             </div>
           </TabPanel>
-        </TabContext>
-      </Box>
     </TabPanel>
-    </TabContext>
+    </div>
   );
 }
