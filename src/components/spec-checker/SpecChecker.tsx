cc40151293c7b2d9bd23f04555025d21f53c1dd6
splex7 <splex7@gmail.com>
2022-12-16 14:07:15 +0900

    Fix dark/light fab loc

--- src/components/spec-checker/SpecChecker.tsx
+++ src/components/spec-checker/SpecChecker.tsx
@@ -85,8 +85,9 @@ export default function SpecChecker() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
 
-		<div className="mx-auto font-extrabold text-3xl md:text-5xl pt-5" ><center>ISO Bunker Fuel Specification</center></div> 
-    <div className='mx-auto'  style={{maxWidth: 1120, paddingTop: 30}}>
+      <div className="mx-auto font-extrabold text-3xl md:text-5xl pt-5" ><center>ISO Bunker Fuel Specification</center>
+     </div> 
+      <div className='mx-auto' style={{ maxWidth: 1120, paddingTop: 30 }}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <Tabs  value={value} onChange={handleChange} aria-label="lab API tabs example">
             <Tab label="Simplified" {...a11yProps(0)} />
@@ -99,7 +100,7 @@ export default function SpecChecker() {
         <TabPanel value={value} index={1}>
             <SpecTable />
         </TabPanel>
-      <Fab color="primary" size="small"  aria-label="add" sx={{position:'absolute', right: '2%', top: '6%'}} onClick={colorMode.toggleColorMode} >
+        <Fab color="primary" size="small"  aria-label="add" onClick={colorMode.toggleColorMode} >
         {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
       </Fab>
     </div>
